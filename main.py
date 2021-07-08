8f371f1797c9a0c5cbb79ad79ce9f20b5b8cece1
Dmitry1515 <chuvilind05@mail.ru>
2021-07-08 14:24:17 +0500

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.storage.jsonstore import JsonStore
 from kivy.uix.textinput import TextInput
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.togglebutton import ToggleButton
-# import bluetooth
+import bluetooth
 class MainApp(App):
 
     def on_start(self):
